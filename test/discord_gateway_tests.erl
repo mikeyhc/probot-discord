3e0a5074207eeed2425f3954b5e578dc341cbd58
mikeyhc <mikeyhc@atmosia.net>
2019-07-26 18:30:23 +0200

    added a new test

--- test/discord_gateway_tests.erl
+++ test/discord_gateway_tests.erl
@@ -11,3 +11,21 @@ decode_payload_test_() ->
     lists:map(fun({In, Out}) ->
                       ?_assertEqual(Out, discord_gateway:decode_payload(In))
               end, Set).
+
+%handle_message_test_() ->
+%    Set = [{{0,
+%             #payload{op=0, d=#{<<"content">> => <<"">>}},
+%             #{session_id => 0}},
+%            #{session_id => 0}}
+%          ],
+%    F = fun({{A, B, C}, Out}) ->
+%                ?_assertEqual(Out, discord_gateway:handle_message(A, B, C))
+%        end,
+%    lists:map(F, Set).
+
+make_heartbeat_test_() ->
+    Set = [{#payload{d = #{<<"heartbeat_interval">> => 100}, s = 0},
+            #{interval => 100, seq => 0}}],
+    lists:map(
+      fun({I, O}) -> ?_assertEqual(discord_gateway:make_heartbeat(I), O) end,
+      Set).
